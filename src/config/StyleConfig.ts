6f6378f8c74f7d1a97ea7980376b3a1e0f6530cb
koiketakayuki <koike.takayuki0907@gmail.com>
2018-08-19 23:45:19 +0900

    Add style config class

--- src/config/StyleConfig.ts
+++ src/config/StyleConfig.ts
@@ -5,6 +5,13 @@ export interface StyleConfig {
   secondary: Color;
   white: Color;
   disabled: Color;
+  fontColor: Color;
+  disabledFontColor: Color;
+  heading1: FontSizeProperty<string>;
+  heading2: FontSizeProperty<string>;
+  heading3: FontSizeProperty<string>;
+  heading4: FontSizeProperty<string>;
+  heading5: FontSizeProperty<string>;
   fontSizeLarge: FontSizeProperty<string>;
   fontSizeMedium: FontSizeProperty<string>;
   fontSizeSmall: FontSizeProperty<string>;
