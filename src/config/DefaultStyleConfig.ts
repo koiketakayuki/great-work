edcf777793c1783cd72d8e5ce0f886b3ddb0169c
koiketakayuki <koike.takayuki0907@gmail.com>
2018-08-21 01:28:15 +0900

    Update StyleConfig API

--- src/config/DefaultStyleConfig.ts
+++ src/config/DefaultStyleConfig.ts
@@ -1,18 +1,76 @@
-import { StyleConfig } from './StyleConfig';
+import { StyleConfig, ColorType, ColorContext } from './StyleConfig';
+import { Color } from 'csstype';
 
 export const DefaultStyleConfig: StyleConfig = {
   primary: '#00A5E1',
-  secondary: '#efefef',
+  secondary: '#47c747',
   white: '#ffffff',
   disabled: '#ababab',
-  disabledFontColor: '#888888',
+  error: '#ffedf2',
   fontColor: '#333333',
-  heading1: '3rem',
-  heading2: '2.6rem',
-  heading3: '2.2rem',
-  heading4: '1.8rem',
+  disabledFontColor: '#888888',
+  errorFontColor: '#ff0000',
+  borderColor: 'red',
+  heading1: '3.0rem',
+  heading2: '2.2rem',
+  heading3: '1.8rem',
+  heading4: '1.6rem',
   heading5: '1.4rem',
-  fontSizeLarge: '1.2rem',
-  fontSizeMedium: '1rem',
-  fontSizeSmall: '0.8rem',
+  fontSizeLarge: '1.4rem',
+  fontSizeMedium: '1.2rem',
+  fontSizeSmall: '1.0rem',
+
+  getColor(type: ColorType): Color {
+    if (type === 'default') {
+      return this.fontColor;
+    }
+
+    if (type === 'secondary') {
+      return this.secondary;
+    }
+
+    if (type === 'error') {
+      return this.errorFontColor;
+    }
+
+    if (type === 'disabled') {
+      return this.disabled;
+    }
+
+    return this.primary;
+  },
+  getColorContext(type: ColorType): ColorContext {
+    if (type === 'default') {
+      return {
+        color: this.fontColor,
+        backgroundColor: 'transparent',
+      };
+    }
+
+    if (type === 'secondary') {
+      return {
+        color: this.white,
+        backgroundColor: this.secondary,
+      };
+    }
+
+    if (type === 'error') {
+      return {
+        color: this.errorFontColor,
+        backgroundColor: this.error,
+      };
+    }
+
+    if (type === 'disabled') {
+      return {
+        color: this.disabledFontColor,
+        backgroundColor: this.disabled,
+      };
+    }
+
+    return {
+      color: this.white,
+      backgroundColor: this.primary,
+    };
+  },
 };
--- src/config/StyleConfig.ts
+++ src/config/StyleConfig.ts
@@ -1,12 +1,18 @@
 import { Color, FontSizeProperty } from 'csstype';
 
+export type ColorType = 'primary' | 'secondary' | 'error' | 'disabled' | 'default';
+export type ColorContext = { color: Color; backgroundColor: Color };
+
 export interface StyleConfig {
   primary: Color;
   secondary: Color;
   white: Color;
   disabled: Color;
+  error: Color;
   fontColor: Color;
   disabledFontColor: Color;
+  errorFontColor: Color;
+  borderColor: Color;
   heading1: FontSizeProperty<string>;
   heading2: FontSizeProperty<string>;
   heading3: FontSizeProperty<string>;
@@ -15,4 +21,7 @@ export interface StyleConfig {
   fontSizeLarge: FontSizeProperty<string>;
   fontSizeMedium: FontSizeProperty<string>;
   fontSizeSmall: FontSizeProperty<string>;
+
+  getColor(type?: ColorType): Color;
+  getColorContext(type?: ColorType): ColorContext;
 }
