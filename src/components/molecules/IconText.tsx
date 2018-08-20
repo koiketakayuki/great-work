bacdceb0f29188f1843482aa89a0eac52f357287
koiketakayuki <koike.takayuki0907@gmail.com>
2018-08-21 01:31:19 +0900

    Add IconText component

---
+++ src/components/molecules/IconText.tsx
@@ -0,0 +1,33 @@
+import * as React from 'react';
+import { Row } from '../layout/Row';
+import { FixedCell, FlexCell } from '../layout/Cell';
+import { Icon } from '../atoms/Icon';
+import { Text } from '../atoms/Text';
+import { StyleConfig } from '../../config/StyleConfig';
+import { Container } from '../layout/Container';
+import { CursorProperty, Color } from 'csstype';
+
+export interface IconTextProps {
+  icon: string;
+  text: string;
+  color?: Color;
+  cursor?: CursorProperty;
+  hover?: object;
+  onClick?: React.MouseEventHandler;
+  config: StyleConfig;
+}
+
+export const IconText = (props: IconTextProps) => (
+  <Row hover={props.hover} cursor={props.cursor} onClick={props.onClick}>
+    <FixedCell>
+      <Row>
+        <Icon name={props.icon} config={props.config} color={props.color}/>
+      </Row>
+    </FixedCell>
+    <FlexCell>
+      <Container padding="0 4px">
+        <Text config={props.config} color={props.color}>{props.text}</Text>
+      </Container>
+    </FlexCell>
+  </Row>
+);
