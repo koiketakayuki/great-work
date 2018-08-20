7399f04d6c7873733c0fdf0ca797d7be537b39b5
koiketakayuki <koike.takayuki0907@gmail.com>
2018-08-21 01:30:43 +0900

    Refactor ErrorMessage component

--- src/components/atoms/ErrorMessage.tsx
+++ src/components/atoms/ErrorMessage.tsx
@@ -1,14 +1,13 @@
 import * as React from 'react';
 import { StyleConfig } from '../../config/StyleConfig';
 import { Text } from './Text';
-import { FontSizeProperty } from 'csstype';
 
-export interface ErrorMessageProp {
+export interface ErrorMessageProps {
   onChange?: (value: string, errorMessage?: string) => void;
   validator?: (value: string) => string | undefined;
   config: StyleConfig;
   children: React.ReactNode;
 }
 
-export const ErrorMessage = (props: ErrorMessageProp) =>
-  <Text fontSize={props.config.fontSizeSmall} color={props.config.errorFontColor} config={props.config}>{props.children}</Text>;
+export const ErrorMessage = (props: ErrorMessageProps) =>
+  <Text fontSize={props.config.fontSizeSmall} color={props.config.getColor('error')} config={props.config}>{props.children}</Text>;
