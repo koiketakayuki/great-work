2febb51a041e9ab5ff482f2201f2d33eeeddf042
koiketakayuki <koike.takayuki0907@gmail.com>
2018-08-21 01:32:57 +0900

    Refactor Link component

--- src/components/atoms/Link.tsx
+++ src/components/atoms/Link.tsx
@@ -1,8 +1,8 @@
 import * as React from 'react';
-import { Text, TextProp } from './Text';
+import { Text, TextProps } from './Text';
 import { StyleConfig } from '../../config/StyleConfig';
 
-export interface LinkProp {
+export interface LinkProps {
   href: string;
   target?: string;
   disabled?: boolean;
@@ -10,10 +10,10 @@ export interface LinkProp {
   config: StyleConfig;
 }
 
-export const Link = (props: LinkProp) =>
+export const Link = (props: LinkProps) =>
   <Text {...(getLinkStyle(props.config, props.href, props.target, props.disabled))}>{props.children}</Text>;
 
-function getLinkStyle(config: StyleConfig, href: string, target?: string, disabled?: boolean): TextProp {
+function getLinkStyle(config: StyleConfig, href: string, target?: string, disabled?: boolean): TextProps {
   if (disabled) {
     return getDisabledStyle(config);
   }
@@ -21,12 +21,11 @@ function getLinkStyle(config: StyleConfig, href: string, target?: string, disabl
   return getDefaultStyle(config, href, target);
 }
 
-function getDisabledStyle(config: StyleConfig): TextProp {
+function getDisabledStyle(config: StyleConfig): TextProps {
   return {
     config,
     tag: 'div',
     fontSize: config.fontSizeMedium,
-    lineHeight: 1.0,
     fontWeight: 'normal',
     decoration: 'none',
     cursor: 'not-allowed',
@@ -34,14 +33,13 @@ function getDisabledStyle(config: StyleConfig): TextProp {
   };
 }
 
-function getDefaultStyle(config: StyleConfig, href: string, target?: string): TextProp {
+function getDefaultStyle(config: StyleConfig, href: string, target?: string): TextProps {
   return {
     config,
     href,
     target,
     tag: 'a',
     fontSize: config.fontSizeMedium,
-    lineHeight: 1.0,
     fontWeight: 'normal',
     decoration: 'underline',
     cursor: 'pointer',
