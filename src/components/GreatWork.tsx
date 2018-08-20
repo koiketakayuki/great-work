b30b18ab500b46dfa01c35741c84ef7b860ea0db
koiketakayuki <koike.takayuki0907@gmail.com>
2018-08-21 01:30:06 +0900

    Remove Unecessary files

--- src/components/GreatWork.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-
-export interface HelloProps { name: string; }
-
-export class GreatWork extends React.Component<HelloProps> {
-  render() {
-    return <h1>GreatWork {this.props.name}!</h1>;
-  }
-}
--- src/components/layout/Stack.tsx
+++
@@ -1,3 +0,0 @@
-import * as React from 'react';
-
-export const Stack = (props: { children?: React.ReactNode }) => <div>{props.children}</div>;
