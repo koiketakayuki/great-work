a76c7b385ba5bce81abd371af8ef05aa586ea99a
koiketakayuki <koike.takayuki0907@gmail.com>
2018-08-22 00:34:40 +0900

    Convert HeadingLevel to number type

--- src/components/atoms/Heading.tsx
+++ src/components/atoms/Heading.tsx
@@ -1,82 +1,83 @@
 import * as React from 'react';
-import { Text, TextProps } from './Text';
+import { Text, TextProps, TextTag } from './Text';
 import { StyleConfig } from '../../config/StyleConfig';
 
-export type HeadingLevel = '1' | '2' | '3' | '4' | '5';
+export type HeadingLevel = 1 | 2 | 3 | 4 | 5;
 
 export interface HeadingProps {
+  tag?: TextTag;
   level: HeadingLevel;
   children: React.ReactNode;
   config: StyleConfig;
 }
 
 export const Heading = (props: HeadingProps) =>
-  <Text {...(getHeadingStyle(props.config, props.level))}>{props.children}</Text>;
+  <Text {...(getHeadingStyle(props.config, props))}>{props.children}</Text>;
 
-function getHeadingStyle(config: StyleConfig, level: HeadingLevel): TextProps {
-  if (level === '2') {
-    return getHeading2Style(config);
+function getHeadingStyle(config: StyleConfig, props: HeadingProps): TextProps {
+  if (props.level === 2) {
+    return getHeading2Style(config, props);
   }
 
-  if (level === '3') {
-    return getHeading3Style(config);
+  if (props.level === 3) {
+    return getHeading3Style(config, props);
   }
 
-  if (level === '4') {
-    return getHeading4Style(config);
+  if (props.level === 4) {
+    return getHeading4Style(config, props);
   }
 
-  if (level === '5') {
-    return getHeading5Style(config);
+  if (props.level === 5) {
+    return getHeading5Style(config, props);
   }
 
-  return getHeading1Style(config);
+  return getHeading1Style(config, props);
 }
 
-function getHeading1Style(config: StyleConfig): TextProps {
+function getHeading1Style(config: StyleConfig, props: HeadingProps): TextProps {
   return {
     config,
-    tag: 'h1',
+    tag: props.tag || 'h1',
     fontSize: config.heading1,
     fontWeight: 'bold',
     decoration: 'none',
   };
 }
 
-function getHeading2Style(config: StyleConfig): TextProps {
+function getHeading2Style(config: StyleConfig, props: HeadingProps): TextProps {
   return {
     config,
-    tag: 'h2',
+    tag: props.tag || 'h2',
     fontSize: config.heading2,
     fontWeight: 'bold',
     decoration: 'none',
   };
 }
 
-function getHeading3Style(config: StyleConfig): TextProps {
+function getHeading3Style(config: StyleConfig, props: HeadingProps): TextProps {
   return {
     config,
-    tag: 'h3',
+    tag: props.tag || 'h3',
     fontSize: config.heading3,
     fontWeight: 'bold',
     decoration: 'none',
   };
 }
 
-function getHeading4Style(config: StyleConfig): TextProps {
+function getHeading4Style(config: StyleConfig, props: HeadingProps): TextProps {
   return {
     config,
-    tag: 'h4',
+    tag: props.tag || 'h4',
     fontSize: config.heading4,
     fontWeight: 'bold',
     decoration: 'none',
   };
 }
 
-function getHeading5Style(config: StyleConfig): TextProps {
+function getHeading5Style(config: StyleConfig, props: HeadingProps): TextProps {
   return {
     config,
-    tag: 'h5',
+    tag: props.tag || 'h5',
     fontSize: config.heading5,
     lineHeight: 1.0,
     fontWeight: 'bold',
