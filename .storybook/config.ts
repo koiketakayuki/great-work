043c0d73acb7a5fd80b2e1a893275c12230ecaf8
koiketakayuki <koike.takayuki0907@gmail.com>
2018-08-22 17:45:47 +0900

    Refactor stories

--- .storybook/config.ts
+++ .storybook/config.ts
@@ -1,13 +1,7 @@
 import { configure } from '@storybook/react';
 
 function loadStories() {
-  require('../stories/Text.story');
-  require('../stories/Heading.story');
-  require('../stories/Link.story');
-  require('../stories/CheckBox.story');
-  require('../stories/TextForm.story');
-  require('../stories/Icon.story');
-  require('../stories/IconText.story');
+  require('../stories/Form.story');
 }
 
 configure(loadStories, module);
