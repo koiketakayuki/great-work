72ee46210ba8df21af38477f6cb0ab3bb2db4292
koiketakayuki <koike.takayuki0907@gmail.com>
2018-08-23 01:26:13 +0900

    Remove unecessary prop

--- src/components/layout/Section.tsx
+++ src/components/layout/Section.tsx
@@ -1,17 +1,15 @@
 import * as React from 'react';
 import { Container } from './Container';
 import { Heading } from '../Heading';
-import { StyleConfig } from '../../config/StyleConfig';
 
 export interface SectionProps {
   label: string;
   children: React.ReactNode;
-  config: StyleConfig;
 }
 
 export const Section = (props: SectionProps) => (
   <div>
-    <Heading level={5} config={props.config} tag="div">{props.label}</Heading>
+    <Heading level={5} tag="div">{props.label}</Heading>
     <Container padding="10px 0">
       {props.children}
     </Container>
