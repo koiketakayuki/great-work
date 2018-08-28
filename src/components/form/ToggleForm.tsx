521b4303c1be1654ad26c16e7a250d7d91efff92
d150024 <koike.takayuki@designone.jp>
2018-08-28 15:39:19 +0900

    Fix ToggleForm readonly style

--- src/components/form/ToggleForm.tsx
+++ src/components/form/ToggleForm.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { FormBaseProps, HasLabel } from './Form';
 import { IconText } from '../IconText';
 import { validatable } from './validatable';
-import { StyleConfig } from '../../config/StyleConfig';
+import { StyleConfig, ColorType } from '../../config/StyleConfig';
 import { StyleContext } from '../../config/StyleContext';
 
 export type ToggleFormProps = FormBaseProps<boolean> & HasLabel;
@@ -10,18 +10,20 @@ export type ToggleFormProps = FormBaseProps<boolean> & HasLabel;
 const createIconToggle = (activeIcon: string, inactiveIcon: string) =>
   class extends React.Component<ToggleFormProps> {
 
-    getColorType() {
+    getColorType(): ColorType {
       if (this.props.disabled) {
         return 'disabled';
       }
 
-      if (this.props.value) {
-        return this.props.type;
+      if (this.props.value && !this.props.readonly) {
+        return this.props.type || 'primary';
       }
+
+      return 'default';
     }
 
     onClick = () => {
-      if (this.props.onChange && !this.props.disabled) {
+      if (this.props.onChange && !this.props.disabled && !this.props.readonly) {
         const newValue: boolean = !this.props.value;
         this.props.onChange(newValue);
       }
@@ -37,12 +39,12 @@ const createIconToggle = (activeIcon: string, inactiveIcon: string) =>
       return (
         <IconText
           icon={this.props.value ? activeIcon : inactiveIcon}
-          cursor={this.props.disabled ? 'not-allowed' : 'pointer'}
+          cursor={this.props.disabled ? 'not-allowed' : (this.props.readonly ? 'default' : 'pointer')}
           text={this.props.label}
           onClick={this.onClick}
           onKeyDown={this.onKeyDown}
-          type={this.props.value ? (this.props.type || 'primary') : undefined}
-          hover={this.props.disabled ? {} : { color: config.getColor(this.props.type || 'primary') }}
+          type={this.getColorType()}
+          hover={(this.props.disabled || this.props.readonly) ? {} : { color: config.getColor(this.props.type || 'primary') }}
           tabIndex={this.props.disabled ? undefined : 0}
         />
       );
