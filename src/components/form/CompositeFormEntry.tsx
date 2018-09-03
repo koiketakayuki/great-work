e7f8014918e0394654f45f4f1ca8b8fbda99f99b
koiketakayuki <koike.takayuki0907@gmail.com>
2018-09-03 19:20:01 +0900

    Fix CompositeFormEntry bug

--- src/components/form/CompositeFormEntry.tsx
+++ src/components/form/CompositeFormEntry.tsx
@@ -2,13 +2,38 @@ import * as React from 'react';
 import { FormEntryProps, createFormEntry } from './createFormEntry';
 import { FormProps } from './Form';
 import { Paper } from '../Paper';
+import { FormContext, ContextValue } from './FormContext';
 
 export type CompositeFormEntryProps<T, C> = FormEntryProps<T, C, FormProps<T>> & {
   children: React.ReactNode;
 };
 
 export function CompositeFormEntry<C, T>(entryProps: CompositeFormEntryProps<T, C>) {
-  return createFormEntry<T, C, FormProps<T>>((props) => {
-    return <Paper>{entryProps.children}</Paper>;
-  })(entryProps);
+  return createFormEntry<T, C, FormProps<T>>(props => (
+    <FormContext.Consumer>
+      {context =>
+        <FormContext.Provider value={getNewContext(context, props, entryProps)}>
+          <Paper>
+            {entryProps.children}
+          </Paper>
+        </FormContext.Provider>
+      }
+    </FormContext.Consumer>
+  ))(entryProps);
+}
+
+function getNewContext<C, T>(
+  currentContext: ContextValue<C>,
+  props: FormProps<T>,
+  entryProps: CompositeFormEntryProps<T, C>,
+): ContextValue<T> {
+  return {
+    value: props.value,
+    disabled: props.disabled || currentContext.disabled,
+    readonly: props.readonly || currentContext.readonly,
+    update: (value: T, hasError: boolean) => {
+      const newValue: C = Object.assign({}, currentContext.value, { [entryProps.id]: value });
+      currentContext.update(newValue, hasError);
+    },
+  };
 }
