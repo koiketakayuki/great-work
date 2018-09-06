122c4994fcd93245cfe772bead84ede0dce8f666
koiketakayuki <koike.takayuki0907@gmail.com>
2018-09-06 20:12:52 +0900

    Update export

--- src/components/GreatWork.ts
+++ src/components/GreatWork.ts
@@ -1,5 +1,45 @@
 import { Button } from './Button';
+import { Heading } from './Heading';
+import { Icon } from './Icon';
+import { IconButton } from './IconButton';
+import { IconText } from './IconText';
+import { Link } from './Link';
+import { Paper } from './Paper';
+import { Text } from './Text';
+import { CheckList } from './form/CheckList';
+import { ErrorMessage } from './form/ErrorMessage';
+import { FormContainer } from './form/FormContainer';
+import { ImageForm } from './form/ImageForm';
+import { FormItem } from './form/FormItem';
+import { ListForm } from './form/ListForm';
+import { PasswordForm } from './form/PasswordForm';
+import { RadioButton } from './form/RadioButton';
+import { RadioButtons } from './form/RadioButtons';
+import { SelectBox } from './form/SelectBox';
+import { TextArea } from './form/TextArea';
+import { TextForm } from './form/TextForm';
+import { Section } from './layout/Section';
 
 export const GreatWork = {
   Button,
+  Heading,
+  Icon,
+  IconButton,
+  IconText,
+  Link,
+  Paper,
+  Text,
+  CheckList,
+  ErrorMessage,
+  FormContainer,
+  FormItem,
+  ImageForm,
+  ListForm,
+  PasswordForm,
+  RadioButton,
+  RadioButtons,
+  SelectBox,
+  TextArea,
+  TextForm,
+  Section,
 };
