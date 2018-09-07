ea92f3b25c348ad9033ab49e5694d061acb03376
d150024 <koike.takayuki@designone.jp>
2018-09-07 11:44:01 +0900

    Update export

--- src/components/GreatWork.ts
+++ src/components/GreatWork.ts
@@ -1,45 +1,21 @@
-import { Button } from './Button';
-import { Heading } from './Heading';
-import { Icon } from './Icon';
-import { IconButton } from './IconButton';
-import { IconText } from './IconText';
-import { Link } from './Link';
-import { Paper } from './Paper';
-import { Text } from './Text';
-import { CheckList } from './form/CheckList';
-import { ErrorMessage } from './form/ErrorMessage';
-import { FormContainer } from './form/FormContainer';
-import { ImageForm } from './form/ImageForm';
-import { FormItem } from './form/FormItem';
-import { ListForm } from './form/ListForm';
-import { PasswordForm } from './form/PasswordForm';
-import { RadioButton } from './form/RadioButton';
-import { RadioButtons } from './form/RadioButtons';
-import { SelectBox } from './form/SelectBox';
-import { TextArea } from './form/TextArea';
-import { TextForm } from './form/TextForm';
-import { Section } from './layout/Section';
-
-export const GreatWork = {
-  Button,
-  Heading,
-  Icon,
-  IconButton,
-  IconText,
-  Link,
-  Paper,
-  Text,
-  CheckList,
-  ErrorMessage,
-  FormContainer,
-  FormItem,
-  ImageForm,
-  ListForm,
-  PasswordForm,
-  RadioButton,
-  RadioButtons,
-  SelectBox,
-  TextArea,
-  TextForm,
-  Section,
-};
+export { Button } from './Button';
+export { Heading } from './Heading';
+export { Icon } from './Icon';
+export { IconButton } from './IconButton';
+export { IconText } from './IconText';
+export { Link } from './Link';
+export { Paper } from './Paper';
+export { Text } from './Text';
+export { CheckList } from './form/CheckList';
+export { ErrorMessage } from './form/ErrorMessage';
+export { FormContainer } from './form/FormContainer';
+export { ImageForm } from './form/ImageForm';
+export { FormItem } from './form/FormItem';
+export { ListForm } from './form/ListForm';
+export { PasswordForm } from './form/PasswordForm';
+export { RadioButton } from './form/RadioButton';
+export { RadioButtons } from './form/RadioButtons';
+export { SelectBox } from './form/SelectBox';
+export { TextArea } from './form/TextArea';
+export { TextForm } from './form/TextForm';
+export { Section } from './layout/Section';
