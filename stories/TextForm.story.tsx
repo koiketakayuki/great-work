4b39020103669423da350528da26da16a5104cc6
d150024 <koike.takayuki@designone.jp>
2018-09-07 17:47:20 +0900

    Remove unecessary code

--- stories/TextForm.story.tsx
+++ stories/TextForm.story.tsx
@@ -4,9 +4,7 @@ import { withInfo } from '@storybook/addon-info';
 import { TextForm } from '../src/components/form/TextForm';
 
 const story = storiesOf('TextForm', module);
-const lengthValidator = (value: string) => value.length < 10 ? 'value must be greater than 10 characters' : undefined;
 const onChange = (value: string) => window.alert(value);
-const onBlur = () => window.alert('Blur');
 
 story.addDecorator(withInfo({ inline: true }));
 story.add('normal', () => <TextForm value="Test"/>);
