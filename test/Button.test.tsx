bfc891b8f8d7d10a11245324084017d56e74da88
d150024 <koike.takayuki@designone.jp>
2018-09-07 13:23:24 +0900

    Add Button test

--- test/Button.test.tsx
+++ test/Button.test.tsx
@@ -1,20 +1,73 @@
 import * as React from 'react';
 import { shallow } from 'enzyme';
 
-import { Button } from '../src/components/GreatWork';
+import { Button, Paper } from '../src/components/GreatWork';
+import { Container } from '../src/components/layout/Container';
+import { FloatLevel } from '../src/lib/getFloatStyle';
 
 describe('Button', () => {
-  test('onClick is called when Button is clicked', () => {
+  it('child element is rendered', () => {
+    const childElement = 'test';
+    const wrapper = shallow(<Button>{childElement}</Button>);
+    expect(wrapper.find(Container).children().text()).toBe(childElement);
+  });
+
+  it('onClick is called when Button is clicked', () => {
     const onClickSpy = jest.fn();
     const wrapper = shallow(<Button onClick={onClickSpy}>test</Button>);
     wrapper.simulate('click');
     expect(onClickSpy).toBeCalled();
   });
 
-  test('onClick is not called when Button is clicked and disabled', () => {
+  it('onClick is not called when Button is clicked and disabled', () => {
     const onClickSpy = jest.fn();
     const wrapper = shallow(<Button onClick={onClickSpy} disabled={true}>test</Button>);
     wrapper.simulate('click');
     expect(onClickSpy).not.toBeCalled();
+  });
+
+  it('has Container', () => {
+    const wrapper = shallow(<Button>test</Button>);
+    expect(wrapper.find(Container).length).toBe(1);
+  });
+
+  it('padding is passed to Container', () => {
+    const padding = '8px';
+    const wrapper = shallow(<Button padding={padding}>test</Button>);
+    const container = wrapper.find(Container);
+    expect(container.props().padding).toBe(padding);
+  });
+
+  it('has Paper', () => {
+    const wrapper = shallow(<Button>test</Button>);
+    expect(wrapper.find(Paper).length).toBe(1);
+  });
+
+  it('type is passed to Paper', () => {
+    const type = 'secondary';
+    const wrapper = shallow(<Button type={type}>test</Button>);
+    expect(wrapper.find(Paper).props().type).toBe(type);
+  });
+
+  it('type passed to Paper becomes disabled when Button is disabled', () => {
+    const type = 'secondary';
+    const wrapper = shallow(<Button type={type} disabled={true}>test</Button>);
+    expect(wrapper.find(Paper).props().type).toBe('disabled');
+  });
+
+  it('float is passed to Paper', () => {
+    const floatLevel: FloatLevel = 4;
+    const wrapper = shallow(<Button float={floatLevel}>test</Button>);
+    expect(wrapper.find(Paper).props().float).toBe(floatLevel);
+  });
+
+  it('cursor is pointer', () => {
+    const wrapper = shallow(<Button>test</Button>);
+    expect(wrapper.find(Paper).props().cursor).toBe('pointer');
+  });
+
+  it('cursor becomes "not-allowed" when Button is diabled', () => {
+    const wrapper = shallow(<Button disabled={true}>test</Button>);
+    expect(wrapper.find(Paper).props().cursor).toBe('not-allowed');
   });
 });
